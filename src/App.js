bbdddd37ef82e82fe6895406cf02414fab008abf
Kevin <kevin26428@gmail.com>
2022-01-11 00:39:36 -0600

    add TODO

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// TODO: make penpal connection with iframe child
+// TODO: grab index file from other server and mock backend client
+// TODO: ability mock multiple test users (different windows?)
+// TODO: hot reloading of user code?
+// TODO: have sample user code for testing locally (in our dev env)?
+
 function App() {
   return (
     <div>
