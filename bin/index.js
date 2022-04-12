74a2c51a5dbc37e147537064eb4e300f39df9974
kevin tang <kevin26428@gmail.com>
2022-04-12 11:28:40 -0500

    add linting

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,26 @@
 #! /usr/bin/env node
 
 const http = require('http');
-const path = require('path')
-var express = require('express');
+const path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 const port = process.env.PORT || 3000;
 
 fs = require('fs');
-var p = path.join(__dirname, "../", "index.html")
 
-console.log(p)
+const p = path.join(__dirname, '../', 'index.html');
 
-fs.readFile(p, function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+console.log(p);
+
+fs.readFile(p, (err, html) => {
+  if (err) {
+    throw err;
+  }
+  http.createServer((request, response) => {
+    response.writeHeader(200, { 'Content-Type': 'text/html' });
+    response.write(html);
+    response.end();
+  }).listen(8000);
 });
