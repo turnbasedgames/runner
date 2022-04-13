4da3892d8ad4a4be05fedeb2d240a6d9bbc0df83
Kevin <kevin26428@gmail.com>
2022-04-13 14:54:55 -0500

    create cards and update todos

--- bin/localrunner.js
+++ bin/localrunner.js
@@ -2,19 +2,19 @@
 const { program, Option } = require('commander');
 const { stringIsAValidUrl } = require('../src/util');
 const { setupFrontends } = require('../src/setupFrontends');
-// TODO: cors on everything
-// TODO: setup dev environment option for a local dummy user frontend and backend
-// TODO: handle when ports are taken in elegant way
+// TODO: MAIN-82 cors on everything
+// TODO: MAIN-83 setup dev environment option for a local dummy user frontend and backend
+// TODO: MAIN-84 handle when ports are taken in elegant way
 // (react-scripts start prompts user with Y/n question to auto choose available port)
 const { setupServer } = require('../src/setupServer');
 
 program
   .addOption(new Option('-t, --tbg-frontend-url <tbgFrontendUrl>').hideHelp())
   .addOption(new Option('-e, --empty-backend').hideHelp())
-  // TODO: tbg frontend needs to query for this value
+  // TODO: MAIN-85 tbg frontend needs to query for this value
   .option('-f, --frontend-url <frontendUrl>', 'if you are already serving your frontend in a dev environment (e.g. React), you can specify the url here')
-  // TODO: we need to use a logger instead of console.log and add debug log outputs everywhere
-  .option('-d, --debug', 'print debug logs to stdout');
+  .option('-d, --debug', 'print debug logs to stdout'); // TODO: MAIN-86 we need to use a logger instead of console.log and add debug log outputs everywhere
+
 program.parse();
 const options = program.opts();
 
