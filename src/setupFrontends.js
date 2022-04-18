b32e782002dfa64013b5455518cef6986a23145d
Kevin <kevin26428@gmail.com>
2022-04-17 22:32:22 -0500

    fix serving frontends from build

--- src/setupFrontends.js
+++ src/setupFrontends.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const {
   buildPath,
   userFrontendPath,
 } = require('../config/paths');
 
+const setupFrontendService = (currentUrl, frontendPath, port) => {
+  if (currentUrl) {
+    return;
+  }
+  const app = express();
+  app.use(cors());
+  app.use(express.static(frontendPath));
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(frontendPath, 'index.html'));
+  });
+  const server = app.listen(port, () => {
+    const url = `http://localhost:${server.address().port}`;
+    console.log(`serving ${frontendPath} at ${url}`);
+  });
+};
+
 module.exports = {
   setupFrontends({ frontendUrl, tbgFrontendUrl }) {
-    const setupFrontendService = (currentUrl, path, port) => {
-      if (currentUrl) {
-        return;
-      }
-      const app = express();
-      app.use(cors());
-      app.use(express.static(path));
-      const server = app.listen(port, () => {
-        const url = `http://localhost:${server.address().port}`;
-        console.log(`serving ${path} at ${url}`);
-      });
-    };
-
     setupFrontendService(tbgFrontendUrl, buildPath, 3100);
     setupFrontendService(frontendUrl, userFrontendPath, 3101);
   },
